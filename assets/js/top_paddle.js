f13f7f854e507251c6dd29d5744959d7db42a265
Chris Marsh <cmhilfiker@gmail.com>
2020-06-01 14:33:43 +0000

    Add top paddle

---
+++ assets/js/top_paddle.js
@@ -0,0 +1,19 @@
+
+//Class to make paddle size
+export default class TopPaddle {
+    constructor(gameWidth, gameHeight) {
+        this.width = gameWidth/10; 
+        this.height = gameHeight/50; 
+        this.position = {
+            xAxis : gameWidth/2-this.width/2, 
+            yAxis : this.height
+        }
+
+}
+    draw(context) {
+        context.fillStyle = "red";
+        context.fillRect(this.position.xAxis, this.position.yAxis, this.width, this.height);
+
+    }
+
+}
