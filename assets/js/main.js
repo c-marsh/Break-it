a405408ef2359df4223fdb272d4073f74abb6c5c
Chris Marsh <cmhilfiker@gmail.com>
2020-06-01 15:26:30 +0000

    Comment tidy

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
 import Paddle from "./paddle.js";
 import TopPaddle from "./top_paddle.js";
 //Identify canvas
-let canvas = document.getElementById('game');
+let canvas = document.getElementById("game");
 
-//Code for retina/HiDef canvas display from https://www.html5rocks.com/en/tutorials/canvas/hidpi/ 
+//Code for retina/HiDef canvas display from https://www.html5rocks.com/en/tutorials/canvas/hidpi/
 function setupCanvas(canvas) {
   // Get the device pixel ratio, falling back to 1.
   var dpr = window.devicePixelRatio || 1;
@@ -13,7 +13,7 @@ function setupCanvas(canvas) {
   // size * the device pixel ratio.
   canvas.width = rect.width * dpr;
   canvas.height = rect.height * dpr;
-  let context = canvas.getContext('2d');
+  let context = canvas.getContext("2d");
   // Scale all drawing operations by the dpr, so you
   // don't have to worry about the difference.
   context.scale(dpr, dpr);
@@ -22,7 +22,7 @@ function setupCanvas(canvas) {
 
 // Now this line will be the same size on the page
 // but will look sharper on high-DPI devices!
-var context = setupCanvas(document.querySelector('#game'));
+var context = setupCanvas(document.querySelector("#game"));
 context.lineWidth = 5;
 context.beginPath();
 context.moveTo(100, 100);
@@ -33,27 +33,30 @@ context.stroke();
 const gameWidth = document.documentElement.clientWidth;
 const gameHeight = 600;
 
+//Define and draw paddles
 let paddle = new Paddle(gameWidth, gameHeight);
 paddle.draw(context);
-
 let topPaddle = new TopPaddle(gameWidth, gameHeight);
 topPaddle.draw(context);
 
-
-//Build a refresh loop
+//Build a frame refresh loop for the game
 let lastTime = 0;
 
-
 function gameLoop(timestamp) {
-    let deltaTime = timestamp - lastTime;
-    lastTime = timestamp;
-    context.clearRect(0, 0, gameWidth, gameHeight); //Clear screen on refresh
-    paddle.update(deltaTime);
-topPaddle.update(deltaTime);
-    paddle.draw(context);
-topPaddle.draw(context);
+  //Define deltaTime as time elapsed since last frame
+  let deltaTime = timestamp - lastTime;
+  //Update the lastTime to current timestamp
+  lastTime = timestamp;
+  //Clear screen on refresh
+  context.clearRect(0, 0, gameWidth, gameHeight);
+  //Pass time to paddles
+  paddle.update(deltaTime);
+  topPaddle.update(deltaTime);
+  //Redraw paddles
+  paddle.draw(context);
+  topPaddle.draw(context);
 
-    requestAnimationFrame(gameLoop);
+  requestAnimationFrame(gameLoop);
 }
-
+//Execute Game Loop
 gameLoop();
--- assets/js/paddle.js
+++ assets/js/paddle.js
@@ -2,8 +2,10 @@
 //Class to make paddle size
 export default class Paddle {
     constructor(gameWidth, gameHeight) {
+        //Paddle dimensions, relative to canvas size
         this.width = gameWidth/10; 
-        this.height = gameHeight/50; 
+        this.height = gameHeight / 50;
+        //Paddle position, relative to canvas size
         this.position = {
             xAxis : gameWidth/2-this.width/2, 
             yAxis : gameHeight-this.height-10 
@@ -11,11 +13,14 @@ export default class Paddle {
 
 }
     draw(context) {
+        //Paddle styling
         context.fillStyle = "red";
+        //Paddle creation
         context.fillRect(this.position.xAxis, this.position.yAxis, this.width, this.height);
 
     }
-update(deltaTime) {
+    update(deltaTime) {
+    //Prevent division by 0 as a result of no first frame
     if (!deltaTime) return;
 
     this.position.xAxis += 5 / deltaTime; //move 5 pixels per frame
