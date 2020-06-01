e2d54d55ee669139e0d956772f18b857d46488b0
Chris Marsh <cmhilfiker@gmail.com>
2020-06-01 14:11:51 +0000

    Use a different pixel code, add responsive width

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,35 @@
 import Paddle from "./paddle.js";
 let canvas = document.getElementById('game');
-
-// Adjusts pixel resolution for Retina displays: https://stackoverflow.com/questions/35820750/understanding-html-retina-canvas-support
-//Returns: 1 on 'normal' screens, 2 on retina displays
-var PIXEL_RATIO = (function () {
-    var ctx = document.createElement("canvas").getContext("2d"),
-        dpr = window.devicePixelRatio || 1,
-        bsr = ctx.webkitBackingStorePixelRatio ||
-              ctx.mozBackingStorePixelRatio ||
-              ctx.msBackingStorePixelRatio ||
-              ctx.oBackingStorePixelRatio ||
-              ctx.backingStorePixelRatio || 1;
-
-    return dpr / bsr;
-})();
+//Define context
 
 
-function makeCanvasHiPPI(canvas) {
-  canvas.style.width  = canvas.width  + "px";
-  canvas.style.height = canvas.height + "px";
-
-  canvas.width  *= PIXEL_RATIO;
-  canvas.height *= PIXEL_RATIO;
-
-  var context = canvas.getContext('2d');
-  context.scale(PIXEL_RATIO, PIXEL_RATIO)
+function setupCanvas(canvas) {
+  // Get the device pixel ratio, falling back to 1.
+  var dpr = window.devicePixelRatio || 1;
+  // Get the size of the canvas in CSS pixels.
+  var rect = canvas.getBoundingClientRect();
+  // Give the canvas pixel dimensions of their CSS
+  // size * the device pixel ratio.
+  canvas.width = rect.width * dpr;
+  canvas.height = rect.height * dpr;
+  let context = canvas.getContext('2d');
+  // Scale all drawing operations by the dpr, so you
+  // don't have to worry about the difference.
+  context.scale(dpr, dpr);
+  return context;
 }
 
-//Define context
-let context = canvas.getContext('2d');
+// Now this line will be the same size on the page
+// but will look sharper on high-DPI devices!
+var context = setupCanvas(document.querySelector('#game'));
+context.lineWidth = 5;
+context.beginPath();
+context.moveTo(100, 100);
+context.lineTo(200, 200);
+context.stroke();
 
 //Defining playing size
-const gameWidth = 800;
+const gameWidth = document.documentElement.clientWidth;
 const gameHeight = 600;
 
 context.clearRect(0, 0, gameWidth, gameHeight); //Clear screen on refresh
