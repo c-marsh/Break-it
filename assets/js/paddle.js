19049e26a795c69ab1b056915178d61fab8b5d66
Chris Marsh <cmhilfiker@gmail.com>
2020-06-01 13:47:42 +0000

    Positioned correctly - questionable math use

--- assets/js/paddle.js
+++ assets/js/paddle.js
@@ -2,11 +2,11 @@
 //Class to make paddle size
 export default class Paddle {
     constructor(gameWidth, gameHeight) {
-        this.width = 150; //To Do: make responsive by making object
-        this.height = 30; //To Do: make responsive by making object
+        this.width = gameWidth / 20; //To Do: make responsive by making object
+        this.height = gameHeight/200; //To Do: make responsive by making object
         this.position = {
-            xAxis : 6,
-            yAxis : 6
+            xAxis : gameWidth / 6, //To Do: revisit - odd maths
+            yAxis : gameHeight/ 5 + this.height*5 //To Do: revisit - odd maths
         }
 
 }
