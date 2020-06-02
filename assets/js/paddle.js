606356849858d7f4b347ac3f46a93110df825f08
Chris Marsh <cmhilfiker@gmail.com>
2020-06-02 09:49:40 +0000

    Add left limit to paddle movement

--- assets/js/paddle.js
+++ assets/js/paddle.js
@@ -5,9 +5,12 @@ export default class Paddle {
         //Paddle dimensions, relative to canvas size
         this.width = gameWidth/10; 
         this.height = gameHeight / 50;
-        //Paddle position, relative to canvas size
+        
+        //Paddle velocity
         this.maxSpeed = 8;
         this.speed = 0;
+        
+        //Paddle position, relative to canvas size
         this.position = {
             xAxis: gameWidth / 2 - this.width / 2,
             yAxis: gameHeight - this.height - 10
@@ -27,7 +30,9 @@ export default class Paddle {
     //Prevent division by 0 as a result of no first frame
     if (!deltaTime) return;
 
-    this.position.xAxis += this.speed; //move 5 pixels per frame
+        this.position.xAxis += this.speed; 
+        
+        if (this.position.xAxis < 0) this.speed = 0;
 }
 }
 
