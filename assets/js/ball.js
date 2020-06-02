650194e07e6949be5908b148e849f2e98febf63d
Chris Marsh <cmhilfiker@gmail.com>
2020-06-02 12:39:53 +0000

    Refactoring code, rename objects more sensibly

--- assets/js/ball.js
+++ assets/js/ball.js
@@ -1,36 +1,41 @@
 export default class Ball {
   constructor(gameWidth, gameHeight) {
     this.gameWidth = gameWidth;
+
     //Ball dimensions, relative to canvas size
-    this.width = gameWidth / 50;
-    this.height = gameHeight / 50;
+    this.sizeR = gameWidth / 100;
 
     //Ball Start coordinates
-    this.xStart = gameWidth/4;
-    this.yStart = gameHeight / 4;
+    this.positon = { x: 100, y: 100 };
 
-    //Ball Direction
-    this.xMove = 2;
-    this.yMove = -2;
+    //Ball Speed
+    this.speed = { x: 2, y: 2 };
 
-    //Ball velocity
-  //  this.speed = this.xMove + this.yMove;
+    //Ball extremes
+    this.ballR = this.positon.x + this.sizeR;
+    this.ballL = this.positon.x - this.sizeR;
+    this.ballT = this.positon.y + this.sizeR;
+    this.ballB = this.positon.y - this.sizeR;
+
+    //Ball direction
+    this.direction = this.xMove + this.yMove;
   }
 
   //ball
   draw(context) {
-    context.fillStyle = "red";
     context.beginPath();
     //context.arc(x,y,r,sAngle,eAngle,counterclockwise);
-    context.arc(this.xStart, this.yStart, this.width, 0, 2 * Math.PI);
+    context.arc(this.positon.x, this.positon.y, this.sizeR, 0, 2 * Math.PI);
     context.fillStyle = "green";
     context.fill();
   }
-    
+
   update(deltaTime) {
     //Prevent division by 0 as a result of no first frame
-    if (!deltaTime) return;
-      this.xStart += this.xMove;
-      this.yStart += this.yMove;
+    this.positon.x += this.speed.x;
+    this.positon.y += this.speed.y;
+ 
+
+    
   }
 }
