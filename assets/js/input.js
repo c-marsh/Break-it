a9bec4b762c99408f91320f09409d5dc7a3d0b02
Chris Marsh <cmhilfiker@gmail.com>
2020-06-02 10:31:16 +0000

    Comment tidy

--- assets/js/input.js
+++ assets/js/input.js
@@ -1,36 +1,34 @@
-
-
+//Export Keybindings
 export default class KeyBindings {
-    constructor(paddle, topPaddle) {
-        document.addEventListener('keydown', (event) => {
-            switch (event.keyCode) {
-                case 37:
-                    paddle.moveLeft()
-                    topPaddle.moveLeft()
-                    break
-                
-                case 39:
-                    paddle.moveRight()
-                    topPaddle.moveRight()
-                    break
-            }
-            
-        });
-        document.addEventListener('keyup', (event) => {
-            switch (event.keyCode) {
-                case 37:
-                    paddle.moveStop()
-                    topPaddle.moveStop()
-                    break
-                
-                case 39:
-                    paddle.moveStop()
-                    topPaddle.moveStop()
-                    break
-            }
-        });
+  //game keybindings: 37=left, 39=right
+  constructor(paddle, topPaddle) {
+    //Key Depress actions
+    document.addEventListener("keydown", (event) => {
+      switch (event.keyCode) {
+        case 37:
+          paddle.moveLeft();
+          topPaddle.moveLeft();
+          break;
 
-}
+        case 39:
+          paddle.moveRight();
+          topPaddle.moveRight();
+          break;
+      }
+    });
+    //Key Release actions
+    document.addEventListener("keyup", (event) => {
+      switch (event.keyCode) {
+        case 37:
+          paddle.moveStop();
+          topPaddle.moveStop();
+          break;
 
-
+        case 39:
+          paddle.moveStop();
+          topPaddle.moveStop();
+          break;
+      }
+    });
+  }
 }
