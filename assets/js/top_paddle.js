5982a502c34979d0358b0eac76cdcece16454092
Chris Marsh <cmhilfiker@gmail.com>
2020-06-02 10:27:26 +0000

    Add Key functionality // tidy comments

--- assets/js/top_paddle.js
+++ assets/js/top_paddle.js
@@ -1,24 +1,49 @@
-
 //Class to make paddle size
 export default class TopPaddle {
-    constructor(gameWidth, gameHeight) {
-        this.width = gameWidth/10; 
-        this.height = gameHeight/50; 
-        this.position = {
-            xAxis : gameWidth/2-this.width/2, 
-            yAxis : this.height
-        }
-
-}
-    draw(context) {
-        context.fillStyle = "red";
-        context.fillRect(this.position.xAxis, this.position.yAxis, this.width, this.height);
-
-    }
-    update(deltaTime) {
+  constructor(gameWidth, gameHeight) {
+    this.gameWidth = gameWidth;
+    //Paddle dimensions, relative to canvas size
+    this.width = gameWidth / 10;
+    this.height = gameHeight / 50;
+    //Paddle velocity
+    this.maxSpeed = 8;
+    this.speed = 0;
+    //Paddle position, relative to canvas size
+    this.position = {
+      xAxis: gameWidth / 2 - this.width / 2,
+      yAxis: this.height,
+    };
+  } //move left function
+  moveLeft() {
+    this.speed = -this.maxSpeed;
+  }
+  //move Right function
+  moveRight() {
+    this.speed = this.maxSpeed;
+  }
+  //Stop move function
+  moveStop() {
+    this.speed = 0;
+  }
+  draw(context) {
+    //Paddle styling
+    context.fillStyle = "red";
+    //Paddle creation
+    context.fillRect(
+      this.position.xAxis,
+      this.position.yAxis,
+      this.width,
+      this.height
+    );
+  }
+  update(deltaTime) {
+    //Prevent division by 0 as a result of no first frame
     if (!deltaTime) return;
 
-    this.position.xAxis += 5 / deltaTime; //move 5 pixels per frame
-}
-
+    this.position.xAxis += this.speed;
+    //Stopping the paddle if it reaches canvas edge
+    if (this.position.xAxis < 0) this.position.xAxis = 0;
+    if (this.position.xAxis > this.gameWidth - this.width)
+      this.position.xAxis = this.gameWidth - this.width;
+  }
 }
