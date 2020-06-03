4bf5c43d9dba9e2fbb090f7e78cb0a6ad0c47e0f
Chris Marsh <cmhilfiker@gmail.com>
2020-06-03 10:43:41 +0000

    Add Brick class to game

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,6 +2,8 @@ import Paddle from "./paddle.js";
 import TopPaddle from "./top_paddle.js";
 import Ball from "./ball.js";
 import KeyBindings from "./input.js";
+import Brick from "./brick.js";
+
 export default class Game {
   constructor(gameWidth, gameHeight) {
     this.gameWidth = gameWidth;
@@ -12,9 +14,10 @@ export default class Game {
   start() {
     this.paddle = new Paddle(this);
     this.topPaddle = new TopPaddle(this);
-    this.ball = new Ball(this);
+      this.ball = new Ball(this);
+      let brick = new Brick(this, { x: 40, y: 30 });
     //create an array of features in the game
-    this.features = [this.ball, this.paddle, this.topPaddle];
+    this.features = [this.ball, this.paddle, this.topPaddle, brick];
     new KeyBindings(this.paddle, this.topPaddle);
   }
 
