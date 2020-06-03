bc5d6b3b7f059fe25fda3084a657de7e1c9f3fa2
Chris Marsh <cmhilfiker@gmail.com>
2020-06-03 15:33:35 +0000

    fix responsive bricks and responsive brick location

--- assets/js/levels.js
+++ assets/js/levels.js
@@ -4,14 +4,17 @@ import Brick from "./brick.js";
 //create the level
 export function createLevel(game, level) {
     let bricks = [];
+    let gameHeight = game.gameHeight;
+    let gameWidth = game.gameWidth;
 
+//Implement rows as per level layout
     level.forEach((row, rowIndex) => {
         row.forEach((brick, brickIndex) => {
         if (brick === 1) {
             let position = {
-                xAxisBrick: 80 * brickIndex,
-                yAxisBrick: 20 + 24 * rowIndex
-            };
+                xAxisBrick: gameWidth/11*brickIndex,
+                yAxisBrick: gameHeight/2 -  20*rowIndex 
+                };
             bricks.push(new Brick(game, position));
             }
         });
@@ -21,9 +24,9 @@ export function createLevel(game, level) {
 
 //build level maps
 export const level1 = [
-    [0,1,0,1,0,1,0,1,0,1],
-    [0,1,0,1,0,1,0,1,0,1],
-    [0,1,0,1,0,1,0,1,0,1],
-    [0,1,0,1,0,1,0,1,0,1],
-    [0,1,0,1,0,1,0,1,0,1]
+    [0,1,0,1,0,1,0,1,0,1,0],
+    [0,1,0,1,0,1,0,1,0,1,0],
+    [0,1,0,1,0,1,0,1,0,1,0],
+    [0,1,0,1,0,1,0,1,0,1,0],
+    [0,1,0,1,0,1,0,1,0,1,0],
 ];
