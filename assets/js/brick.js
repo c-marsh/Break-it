ed423fa846cae4a74cd750029dddd270834f3310
Chris Marsh <cmhilfiker@gmail.com>
2020-06-03 10:43:23 +0000

    initial commit / brick class

---
+++ assets/js/brick.js
@@ -0,0 +1,26 @@
+export default class Brick {
+    constructor(game) {
+        this.gameWidth = game.gameWidth
+        this.gameHeight = game.gameHeight
+        this.game = game;
+        //Paddle dimensions, relative to canvas size
+        this.width = this.gameWidth / 10; 
+        this.height = this.gameHeight / 50;
+        //Position
+         this.position = {
+      xAxisBrick: this.gameWidth / 2 - this.width / 2,
+      yAxisBrick: this.height
+    };
+    }  
+    draw(context){
+        //Paddle styling
+        context.fillStyle = "blue";
+        //Paddle creation
+        context.fillRect(this.position.xAxisBrick, this.position.yAxisBrick, this.width, this.height);
+
+    }
+    update(deltaTime){
+
+    }
+  
+}
