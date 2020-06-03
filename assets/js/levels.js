2c483e3523c921d57a70b8774b0d7bd3bc169527
Chris Marsh <cmhilfiker@gmail.com>
2020-06-03 15:47:18 +0000

    prettify

--- assets/js/levels.js
+++ assets/js/levels.js
@@ -3,30 +3,30 @@ import Brick from "./brick.js";
 
 //create the level
 export function createLevel(game, level) {
-    let bricks = [];
-    let gameHeight = game.gameHeight;
-    let gameWidth = game.gameWidth;
+  let bricks = [];
+  let gameHeight = game.gameHeight;
+  let gameWidth = game.gameWidth;
 
-//Implement rows as per level layout
-    level.forEach((row, rowIndex) => {
-        row.forEach((brick, brickIndex) => {
-        if (brick === 1) {
-            let position = {
-                xAxisBrick: gameWidth/11*brickIndex,
-                yAxisBrick: gameHeight/2 -  20*rowIndex 
-                };
-            bricks.push(new Brick(game, position));
-            }
-        });
+  //Implement rows as per level layout
+  level.forEach((row, rowIndex) => {
+    row.forEach((brick, brickIndex) => {
+      if (brick === 1) {
+        let position = {
+          xAxisBrick: (gameWidth / 11) * brickIndex,
+          yAxisBrick: gameHeight / 2 - 20 * rowIndex,
+        };
+        bricks.push(new Brick(game, position));
+      }
     });
-    return bricks;
+  });
+  return bricks;
 }
 
 //build level maps
 export const level1 = [
-    [0,1,0,1,0,1,0,1,0,1,0],
-    [0,1,0,1,0,1,0,1,0,1,0],
-    [0,1,0,1,0,1,0,1,0,1,0],
-    [0,1,0,1,0,1,0,1,0,1,0],
-    [0,1,0,1,0,1,0,1,0,1,0],
+  [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+  [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+  [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+  [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+  [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
 ];
