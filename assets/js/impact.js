a76cdd7552097a150ec90f192eb8f5db1ac45784
Chris Marsh <cmhilfiker@gmail.com>
2020-06-05 09:49:07 +0000

    define limits of bricks and paddles, create impact if method

--- assets/js/impact.js
+++ assets/js/impact.js
@@ -1,17 +1,46 @@
-export function impact(){
-let ballT = ball.position.y - ball.sizeR;
-let ballB = ball.position.y + ball.sizeR;
-let ballL = ball.position.x - ball.sizeR;
-let ballR = ball.position.x + ball.sizeR;
+export function impact() {
+  //ball measured from centre of ball
+  let ballT = ball.position.y - ball.sizeR;
+  let ballB = ball.position.y + ball.sizeR;
+  let ballL = ball.position.x - ball.sizeR;
+  let ballR = ball.position.x + ball.sizeR;
 
-let rectangleB
-let rectangleT
-let rectangleL
-let rectangleR
+  //rectangles measured from top left
+  let brickB = brick.position.yAxisBrick + brick.height;
+  let brickT = brick.position.yAxisBrick;
+  let brickL = brick.position.xAxisBrick;
+  let brickR = brick.position.xAxisBrick + brick.width;
 
+  let paddleT = paddle.position.yAxis;
+  let paddleB = paddle.position.yAxis + paddle.height;
+  let paddleL = paddle.position.xAxis;
+  let paddleR = paddle.position.xAxis + paddle.width;
 
+  let TopPaddleT = topPaddle.position.yAxis;
+  let TopPaddleB = topPaddle.position.yAxis + paddle.height;
+  let TopPaddleL = topPaddle.position.xAxis;
+  let TopPaddleR = topPaddle.position.xAxis + paddle.width;
 
-
-
-
+    //returns true if ball has come into contact with a brick/paddle surface
+  if (
+    ballB >= TopPaddleT ||
+    paddleT ||
+    (brickT && ballR >= TopPaddleL) ||
+    paddleL ||
+    (brickL && ballL <= TopPaddleR) ||
+    paddleR ||
+    (brickR && ballT <= TopPaddleB) ||
+    paddleB ||
+    brickB
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 }
+
+// if (
+//       ballBottom >= bottomPaddleTop &&
+//       this.position.x >= leftSideOfPaddle &&
+//       this.position.x <= rightSideOfPaddle
+//     )
