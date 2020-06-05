28c768e63606f9b4ca082c490969484121dc57fa
Chris Marsh <cmhilfiker@gmail.com>
2020-06-05 09:08:35 +0000

    initial Impact function commit

---
+++ assets/js/impact.js
@@ -0,0 +1,17 @@
+export function impact(){
+let ballT = ball.position.y - ball.sizeR;
+let ballB = ball.position.y + ball.sizeR;
+let ballL = ball.position.x - ball.sizeR;
+let ballR = ball.position.x + ball.sizeR;
+
+let rectangleB
+let rectangleT
+let rectangleL
+let rectangleR
+
+
+
+
+
+
+}
