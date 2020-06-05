66c3e7a4260c351fe14e704b28b793ac3d047794
Chris Marsh <cmhilfiker@gmail.com>
2020-06-05 12:35:26 +0000

    fix bug with ball direction introduced in previous commit

--- assets/js/impact.js
+++ assets/js/impact.js
@@ -1,4 +1,7 @@
-export function impact(ball, paddle, topPaddle, brick) {
+export function impact(ball,
+    paddle,
+    topPaddle,
+    brick) {
   //ball measured from centre of ball
   let ballT = ball.position.y - ball.sizeR;
   let ballB = ball.position.y + ball.sizeR;
@@ -6,37 +9,28 @@ export function impact(ball, paddle, topPaddle, brick) {
   let ballR = ball.position.x + ball.sizeR;
 
   //rectangles measured from top left
-  let brickB = brick.position.yAxisBrick + brick.height;
-  let brickT = brick.position.yAxisBrick;
-  let brickL = brick.position.xAxisBrick;
-  let brickR = brick.position.xAxisBrick + brick.width;
+//   let brickB = brick.position.yAxisBrick + brick.height;
+//   let brickT = brick.position.yAxisBrick;
+//   let brickL = brick.position.xAxisBrick;
+//   let brickR = brick.position.xAxisBrick + brick.width;
 
   let paddleT = paddle.position.yAxis;
   let paddleB = paddle.position.yAxis + paddle.height;
   let paddleL = paddle.position.xAxis;
   let paddleR = paddle.position.xAxis + paddle.width;
 
-  let TopPaddleT = topPaddle.position.yAxis;
-  let TopPaddleB = topPaddle.position.yAxis + paddle.height;
-  let TopPaddleL = topPaddle.position.xAxis;
-  let TopPaddleR = topPaddle.position.xAxis + paddle.width;
+  let topPaddleT = topPaddle.position.yAxis;
+  let topPaddleB = topPaddle.position.yAxis + paddle.height;
+  let topPaddleL = topPaddle.position.xAxis;
+  let topPaddleR = topPaddle.position.xAxis + paddle.width;
 
     //returns true if ball has come into contact with a brick/paddle surface
-  if (
-    ballB >= TopPaddleT ||
-    paddleT ||
-    (brickT && ballR >= TopPaddleL) ||
-    paddleL ||
-    (brickL && ballL <= TopPaddleR) ||
-    paddleR ||
-    (brickR && ballT <= TopPaddleB) ||
-    paddleB ||
-    brickB
-  ) {
-    return true;
-  } else {
+    if (ballB >= paddleT && ballR >= paddleL && ballL <= paddleR && ballT <= paddleB) return true 
+    else if (ballB >= topPaddleT && ballR >= topPaddleL && ballL <= topPaddleR && ballT <= topPaddleB) return true 
+    //    else if (ballB >= brickT && ballR >= brickL && BallL <= brickR && ballT <= brickB) return true;  
+    else
     return false;
-  }
+  
 }
 
 // if (
