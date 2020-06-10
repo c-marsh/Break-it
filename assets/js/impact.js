ff6e97f482cffd4b60142a77ee3a67a8a251db7f
Chris Marsh <cmhilfiker@gmail.com>
2020-06-10 20:55:57 +0000

    Improve quality of collision detection to take into account ball corners

--- assets/js/impact.js
+++ assets/js/impact.js
@@ -1,23 +1,28 @@
 export function impact(ball, features) {
-  //ball measured from centre of ball
-  let ballT = ball.position.y - ball.sizeR;
-  let ballB = ball.position.y + ball.sizeR;
-  let ballL = ball.position.x - ball.sizeR;
-  let ballR = ball.position.x + ball.sizeR;
-
-  //rectangles measured from top left
-  let featureT = features.position.yAxis;
-  let featureB = features.position.yAxis + features.height;
-  let featureL = features.position.xAxis;
-  let featureR = features.position.xAxis + features.width;
-
-  //returns true if ball has come into contact with a brick/paddle surface
-  if (
-    ballB >= featureT &&
-    ballR >= featureL &&
-    ballL <= featureR &&
-    ballT <= featureB
-  )
+  //Horizontal distances between centre of ball and object
+  let xDistance = Math.abs(
+    ball.position.x - features.position.xAxis - features.width / 2
+  );
+  //Vertical distances between centre of ball and object
+  let yDistance = Math.abs(
+    ball.position.y - features.position.yAxis - features.height / 2
+  );
+  //Return false if the distance is greater half circle of the ball and half the object
+  if (xDistance > features.width / 2 + ball.sizeR) {
+    return false;
+  }
+  if (yDistance > features.height / 2 + ball.sizeR) {
+    return false;
+  }
+  //Return true if the distance is less half circle of the ball and half the object meaning they've collided
+  if (xDistance <= features.width / 2) {
     return true;
-  else return false;
+  }
+  if (yDistance <= features.height / 2) {
+    return true;
+    }
+  //Check for collision at corner using Pythagorean theorem a^2 + b^2 = c^2
+  let dx = xDistance - features.width / 2;
+  let dy = yDistance - features.height / 2;
+  return dx * dx + dy * dy <= ball.sizeR * ball.sizeR;
 }
