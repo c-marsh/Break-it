41e7b73044116dffd71267ed275dd354dd7bd07e
Chris Marsh <cmhilfiker@gmail.com>
2020-06-12 16:22:50 +0000

    Remove trailing spaces

--- assets/js/impact.js
+++ assets/js/impact.js
@@ -4,12 +4,12 @@ export function impact(ball, features) {
   let xDistance = Math.abs(
     ball.position.x - features.position.xAxis - features.width / 2
   );
-    
+
   //Vertical distances between centre of ball and object
   let yDistance = Math.abs(
     ball.position.y - features.position.yAxis - features.height / 2
   );
-    
+
   //Return false if the distance is greater half circle of the ball and half the object
   if (xDistance > features.width / 2 + ball.sizeR) {
     return false;
@@ -17,7 +17,7 @@ export function impact(ball, features) {
   if (yDistance > features.height / 2 + ball.sizeR) {
     return false;
   }
-    
+
   //Return true if the distance is less half circle of the ball and half the object meaning they've collided
   if (xDistance <= features.width / 2) {
     return true;
@@ -25,7 +25,7 @@ export function impact(ball, features) {
   if (yDistance <= features.height / 2) {
     return true;
   }
-    
+
   //Check for collision at corner using Pythagorean theorem a^2 + b^2 = c^2
   let dx = xDistance - features.width / 2;
   let dy = yDistance - features.height / 2;
